9647320937cd5f7236a0bfa3ec69be6b6b00cc29
shrki416 <coach-ahmed@bmsnj.org>
2020-04-18 13:36:39 -0400

    Creating Gatsby Pages

---
+++ src/components/ProductsListing.js
@@ -0,0 +1,47 @@
+import React from "react"
+import Product from "../components/Product"
+import { useStaticQuery, graphql } from "gatsby"
+
+const PRODUCTS_LISTING_QUERY = graphql`
+  query ProductsListingQuery {
+    allShopifyProduct {
+      edges {
+        node {
+          id
+          title
+          images {
+            localFile {
+              childImageSharp {
+                fixed(width: 200) {
+                  ...GatsbyImageSharpFixed_withWebp_tracedSVG
+                }
+              }
+            }
+          }
+          publishedAt(formatString: "YYYY")
+          description
+          descriptionHtml
+          variants {
+            sku
+            id
+            title
+            price
+          }
+        }
+      }
+    }
+  }
+`
+
+const ProductsListing = () => {
+  const { allShopifyProduct } = useStaticQuery(PRODUCTS_LISTING_QUERY)
+  return (
+    <div>
+      {allShopifyProduct.edges.map(edge => (
+        <Product product={edge.node} key={edge.node.id} />
+      ))}
+    </div>
+  )
+}
+
+export default ProductsListing
